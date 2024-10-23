bd8e262d5992f844c475b2674d6a2033e2d6594b
CountToFour <247806@edu.p.lodz.pl>
2024-10-23 19:00:27 +0200

    bazy -> zad1 ppkt 2 sql i postresql

--- Bazy danych/zd1_2.sql
+++ Bazy danych/zd1_2.sql
@@ -4,7 +4,6 @@ LEFT JOIN employees e ON d.department_id = e.department_id
 WHERE e.employee_id IS NULL
 GROUP BY D.department_name, D.department_id;
 
-
 SELECT 
     m.first_name AS manager_first_name,
     m.last_name AS manager_last_name,
@@ -21,7 +20,8 @@ JOIN (
         manager_id IS NOT NULL
     GROUP BY 
         manager_id
-) avg_emp_salary ON m.employee_id = avg_emp_salary.manager_id;
+) avg_emp_salary ON m.employee_id = avg_emp_salary.manager_id
+ORDER BY salary_difference;
 
 
 
@@ -29,7 +29,12 @@ SELECT l.city, CONCAT(l.postal_code, '_', l.country_id,
         UPPER(RIGHT(l.city, 3))
     ) AS code
 FROM locations l
-WHERE l.postal_code ~ '^[0-9]{5}$';  -- Filtruj tylko pięciocyfrowe kody pocztowe
+WHERE l.postal_code ~ '^[0-9]{5}$'; 
+
+-- sql
+-- SELECT l.city, CONCAT(l.postal_code, '_', l.country_id, UPPER(RIGHT(l.city, 3))) AS code
+-- FROM locations l
+-- WHERE l.postal_code LIKE '[0-9][0-9][0-9][0-9][0-9]';
 
 SELECT d.department_name
 FROM departments d
