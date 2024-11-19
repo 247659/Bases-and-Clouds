c58e3d3a2442fdc47f0b994208d714db245ec026
240787 Ruszer Julia <240787@edu.p.lodz.pl>
2024-11-19 12:12:00 +0100

    bazy -> zad2 ppkt1-2 postgressql

---
+++ Bazy danych/Zad2/ppkt1_postgressql.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION count_employees(
+    job_title_fun VARCHAR,
+    department_name_fun VARCHAR,
+    country_name_fun VARCHAR
+) RETURNS INTEGER AS $$
+BEGIN
+    RETURN (
+        SELECT COUNT(e.employee_id)
+        FROM employees e
+        JOIN jobs j ON e.job_id = j.job_id
+        JOIN departments d ON e.department_id = d.department_id
+        JOIN locations l ON d.location_id = l.location_id
+        JOIN countries c ON l.country_id = c.country_id
+        WHERE j.job_title = job_title_fun
+          AND d.department_name = department_name_fun
+          AND c.country_name = country_name_fun
+    );
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT count_employees('Sales Manager', 'Sales', 'United Kingdom');
+
+/*
+SELECT * FROM employees e
+JOIN jobs j ON e.job_id = j.job_id
+JOIN departments d ON e.department_id = d.department_id
+JOIN locations l ON d.location_id = l.location_id
+JOIN countries c ON l.country_id = c.country_id
+WHERE j.job_title = 'Sales Manager'
+  AND d.department_name = 'Sales'
+  AND c.country_name = 'United Kingdom';
+  */
+
